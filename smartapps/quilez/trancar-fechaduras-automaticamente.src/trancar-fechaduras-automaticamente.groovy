9f7aafa76a139b321dca4411cfbeadf40e7df81d
Rodrigo Lopes Quilez <rlquilez@gmail.com>
2015-12-14 09:54:08 -0200

    V2.0

--- smartapps/quilez/trancar-fechaduras-automaticamente.src/trancar-fechaduras-automaticamente.groovy
+++ smartapps/quilez/trancar-fechaduras-automaticamente.src/trancar-fechaduras-automaticamente.groovy
@@ -20,7 +20,7 @@ definition(
     name: "Trancar fechaduras automaticamente",
     namespace: "quilez",
     author: "Rodrigo Quilez",
-    description: "Tranca automaticamente a fechadura quando a porta estiver fechada por X minutos e destranca quando estiver aberta por mais de X segundos.",
+    description: "Tranca automaticamente a fechadura após determinado tempo.",
     category: "Safety & Security",
     iconUrl: "http://www.vaishnavisoftech.com/images/unnamed-2.png",
     iconX2Url: "http://www.vaishnavisoftech.com/images/unnamed-2.png"
@@ -28,20 +28,27 @@ definition(
 
 preferences{
     section("Selecione a fechadura:") {
-        input "lock1", "capability.lock", required: true
+        input (name: "lock1", type: "capability.lock", required: true)
     }
     section("Selecione o sensor da porta:") {
-    	input "contact", "capability.contactSensor", required: true
+    	input (name: "contact", type: "capability.contactSensor", required: true)
     }   
-    section("Trancar fechadura quando a porta estiver fechada por...") {
-        input "minutesLater", "number", title: "Delay (in minutes):", required: true
+    section("Quando a porta for fechada?") {
+		input (name: "closedDoor", title: "Trancar automaticamente?", type: "bool", required: true, defaultValue: true)
+		input (name: "closedDelay", title: "Tempo (em segundos):", type: "number", required: true, defaultValue: 600)      
     }
-    section("Destrancar fechadura quando a porta estiver aberta por...") {
-        input "secondsLater", "number", title: "Delay (in seconds):", required: true
+    section("Quando a porta estiver aberta?") {
+    	input (name: "openedDoor", title: "Destrancar automaticamente?", type: "bool", required: true, defaultValue: true)
+        input (name: "openedDelay", title: "Tempo (em segundos):", type: "number", required: true, defaultValue: 2) 
+	}
+    section("Quando a porta estiver destrancada?") {
+    	input (name: "unlockedDoor", title: "Trancar automaticamente?", type: "bool", required: true, defaultValue: true)
+        input (name: "unlockedDelay", title: "Tempo (em segundos):", type: "number", required: true, defaultValue: 1800) 
     }
     section( "Notificações" ) {
-		input "sendPushMessage", "enum", title: "Enviar notificações de push?", metadata:[values:["Yes", "No"]], required: true
-		input "phoneNumber", "phone", title: "Insira o número de celular que receberá mensagem texto", required: false
+		input (name: "sendEventMessage", title: "Registrar mensagem do evento?", type: "bool", required: true, defaultValue: true)
+		input (name: "sendPushMessage", title: "Enviar notificações de push?", type: "bool", required: true, defaultValue: true)
+		input (name: "phoneNumber", title: "Insira o número de celular que receberá mensagem texto", type: "phone", required: false)
 	}
 }
 
@@ -63,54 +70,111 @@ def initialize(){
 	subscribe(contact, "contact.closed", doorHandler)
 }
 
-def lockDoor(){
-    log.debug "Trancando porta."
-    lock1.lock()
-    log.debug ( "Enviando notificação de push..." ) 
-    //if ( sendPushMessage == "Yes" ) sendPush( "${lock1} locked after ${contact} was closed for ${minutesLater} minutes!!" )
-    if ( sendPushMessage == "Yes" ) sendPush( "${lock1} trancada depois de ${contact} fechada por ${minutesLater} minutos!" )
-    log.debug("Enviando mensagem de texto...")
-    if ( phoneNumber ) sendSms( phoneNumber, "${lock1} trancada depois de ${contact} fechada por ${minutesLater} minutos!" )
+def lockDoorClosed(){
+    if ((contact.latestValue("contact") == "closed") && (lock1.latestValue("lock") == "unlocked")) { // Verifica que a porta esta fechada e destrancada.
+    	log.debug "Trancando fechadura..."
+    	lock1.lock()
+    	log.debug ( "Enviando notificação..." )
+		log.debug ( "CLOSEDDELAY: ${closedDelay}")
+		if (closedDelay >= 60) {
+			def delayTime = (closedDelay / 60)
+			log.debug ( "DELAYTIME: ${delayTime}")
+			send ("${lock1} trancada depois de ${contact} fechada por ${delayTime} minutos!")
+		}
+		else {
+			send ("${lock1} trancada depois de ${contact} fechada por ${closedDelay} segundos!")
+		}
+	}
+	else if (contact.latestValue("contact") == "open") {
+		log.debug "${contact} ESTA ABERTA!"
+	}
+	else (lock1.latestValue("lock")) {
+		log.debug "${lock1} JA ESTA TRANCADA"
+	}
 }
 
-def unlockDoor(){
-    log.debug "Destrancando porta."
-    lock1.unlock()
-    log.debug ( "Enviando Notificação de Push..." ) 
-    if ( sendPushMessage == "Yes" ) sendPush( "${lock1} destrancada despois de ${contact} aberto por ${secondsLater} segundos!" )
-    log.debug("Enviando mensagem de texto...")
-    if ( phoneNumber ) sendSms( phoneNumber, "${lock1} destrancada despois de ${contact} aberto por ${secondsLater} segundos!" )
+def lockDoorNoEvent(){
+    if ((contact.latestValue("contact") == "closed") && (lock1.latestValue("lock") == "unlocked")) { // Verifica que a porta esta fechada e destrancada.
+    	log.debug "Trancando fechadura..."
+    	lock1.lock()
+    	log.debug ( "Enviando notificação..." )
+		log.debug ( "UNLOCKEDDELAY: ${unlockedDelay}")
+		if (unlockedDelay >= 60) {
+			def delayTime = (unlockedDelay / 60)
+			log.debug ( "DELAYTIME: ${delayTime}")
+			send ("${lock1} trancada depois de ${contact} destrancada por ${delayTime} minutos!")
+		}
+		else {
+			send ("${lock1} trancada depois de ${contact} destrancada por ${unlockedDelay} segundos!")
+		}
+	}
+	else if (contact.latestValue("contact") == "open") {
+		log.debug "${contact} ESTA ABERTA!"
+	}
+	else (lock1.latestValue("lock")) {
+		log.debug "${lock1} JA ESTA TRANCADA"
+	}
 }
+
+def unlockDoorOpened(){
+	if ((contact.latestValue("contact") == "open") && (lock1.latestValue("lock") == "locked")) { // Verifica que a porta esta fechada e destrancada.
+    	log.debug "Destrancando fechadura..."
+    	lock1.unlock()
+    	log.debug ( "Enviando notificação..." )
+		log.debug ( "OPENEDDELAY: ${openedDelay}")
+		if (openedDelay >= 60) {
+			def delayTime = (openedDelay / 60)
+			log.debug ( "DELAYTIME: ${delayTime}")
+			send ("${lock1} destrancada despois de ${contact} aberta por ${delayTime} minutos!")
+		}
+		else {
+			send ("${lock1} destrancada despois de ${contact} aberta por ${openedDelay} segundos!")
+		}
+	}
+	else if (contact.latestValue("contact") == "closed") {
+		log.debug "${contact} ESTA FECHADA!"
+	}
+	else (!lock1.latestValue("lock")) {
+		log.debug "${lock1} JA ESTA DESTRANCADA"
+	}
+}
+
 
 def doorHandler(evt){
-    if ((contact.latestValue("contact") == "open") && (evt.value == "locked")) { // If the door is open and a person locks the door then...  
-        def delay = (secondsLater) // runIn uses seconds
-        runIn( delay, unlockDoor )   // ...schedule (in minutes) to unlock...  We don't want the door to be closed while the lock is engaged. 
+	// Caso a porta esteja ABERTA e a fechadura seja TRANCADA por alguma pessoa...
+    if ((contact.latestValue("contact") == "open") && (evt.value == "locked")) {  
+        runIn( openedDelay, unlockDoorOpened )   // ...o sistema irá destrancar a fechadura após o tempo determinado para evitar problemas.
     }
-    else if ((contact.latestValue("contact") == "open") && (evt.value == "unlocked")) { // If the door is open and a person unlocks it then...
-        unschedule( unlockDoor ) // ...we don't need to unlock it later.
+    // Caso a porta esteja FECHADA e a fechadura seja DESTRANCADA por alguma pessoa...
+    else if ((contact.latestValue("contact") == "closed") && (evt.value == "unlocked")) { 
+        runIn( unlockedDelay, lockDoorNoEvent ) // ...o sistema irá trancar a fechadura após o tempo determinado para garantir a segurança.
+    }
+    // Caso a fechadura esteja DESTRANCADA e a porta seja FECHADA por alguma pessoa...
+    else if ((lock1.latestValue("lock") == "unlocked") && (evt.value == "closed")) { 
+        runIn( closedDelay, lockDoorClosed ) // ...o sistema irá trancar a fechadura após o tempo determinado para garantir a segurança.
 	}
-    else if ((contact.latestValue("contact") == "closed") && (evt.value == "locked")) { // If the door is closed and a person manually locks it then...
-        unschedule( lockDoor ) // ...we don't need to lock it later.
-    }   
-    else if ((contact.latestValue("contact") == "closed") && (evt.value == "unlocked")) { // If the door is closed and a person unlocks it then...
-        def delay = (minutesLater * 60) // runIn uses seconds
-        runIn( delay, lockDoor ) // ...schedule (in minutes) to lock.
+}
+
+private send(msg) {
+    if (location.contactBookEnabled) {
+        log.debug("sending notifications to: ${recipients?.size()}")
+        sendNotificationToContacts(msg, recipients)
     }
-    else if ((lock1.latestValue("lock") == "unlocked") && (evt.value == "open")) { // If a person opens an unlocked door...
-        unschedule( lockDoor ) // ...we don't need to lock it later.
+    else {
+		if ((sendEventMessage) && (!sendPushMessage)) {
+	    	log.debug("sending event message")
+			sendNotificationEvent(msg)
+			wait 
+	    }
+        if (sendPushMessage) {
+            log.debug("sending push message")
+            sendPush(msg)
+        }
+        if (phoneNumber) {
+            log.debug("sending text message")
+            sendSms(phoneNumber, msg)
+        }
     }
-    else if ((lock1.latestValue("lock") == "unlocked") && (evt.value == "closed")) { // If a person closes an unlocked door...
-        def delay = (minutesLater * 60) // runIn uses seconds
-        runIn( delay, lockDoor ) // ...schedule (in minutes) to lock.
-	}
-    else { //Opening or Closing door when locked (in case you have a handle lock)
-    	//log.debug "Unlocking the door."
-		//lock1.unlock()
-        //log.debug ( "Sending Push Notification..." ) 
-        ////sendNotificationEvent("Fechadura destravada temporariamente")
-    	//if ( sendPushMessage == "Yes" ) sendPush( "${lock1} unlocked after ${contact} was opened or closed when ${lock1} was locked!" )
-        //log.debug("Sending text message...")
-    	//if ( phoneNumber ) sendSms( phoneNumber, "${lock1} unlocked after ${contact} was opened or closed when ${lock1} was locked!" )
-		}
+
+	log.debug msg
 }
